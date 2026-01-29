5b7bcd0bcf95db87ef41940a6e78895f2e0a163d
Luka <lukacb@al.insper.edu.br>
2026-01-29 16:25:38 -0300

    ex5

--- funcoes.py
+++ funcoes.py
@@ -42,3 +42,21 @@ def conta_pontos(pecas):
         total += peca[0] + peca[1]
     return total
 
+def posicoes_possiveis(mesa, pecas):
+    posicoes = []
+    
+    if len(mesa) == 0:
+        for i in range(len(pecas)):
+            posicoes.append(i)
+        return posicoes
+    
+    ponta_esquerda = mesa[0][0]
+    ponta_direita = mesa[-1][1]
+    
+    for i in range(len(pecas)):
+        peca = pecas[i]
+        if peca[0] == ponta_esquerda or peca[1] == ponta_esquerda or peca[0] == ponta_direita or peca[1] == ponta_direita:
+            posicoes.append(i)
+            
+    return posicoes
+

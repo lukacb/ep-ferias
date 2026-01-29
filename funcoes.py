5d10cd38d1f948f3a85f6f2e1a5274c33b00b90a
Luka <lukacb@al.insper.edu.br>
2026-01-29 15:46:40 -0300

    ex2

--- funcoes.py
+++ funcoes.py
@@ -10,3 +10,20 @@ def cria_pecas():
     shuffle(pecas)
     
     return pecas
+
+def inicia_jogo(num_jogadores, pecas):
+    pecas_copia = pecas.copy()
+    jogadores = {}
+    
+    for i in range(num_jogadores):
+        jogadores[i] = []
+        for _ in range(7):
+            jogadores[i].append(pecas_copia.pop())
+    jogo = {
+        'jogadores': jogadores,
+        'monte': pecas_copia,
+        'mesa': []
+    }
+    
+    return jogo
+

be63c510aa6933a10a82f4f3c71c6fb9d76c06a8
Luka <lukacb@al.insper.edu.br>
2026-01-30 15:47:24 -0300

    tirando while redundante

--- jogo.py
+++ jogo.py
@@ -8,26 +8,6 @@ jogo = inicia_jogo(num_jogadores, pecas)
 jogador_atual = 0
 
 
-jogando = True
-while jogando:
-    print("\nMESA:")
-    mesa_formatada = ""
-    for p in jogo['mesa']:
-        mesa_formatada += f"[{p[0]}|{p[1]}]"
-    print(mesa_formatada if mesa_formatada else "Vazia")
-
-    if jogador_atual == 0:
-        print(f"Jogador: Você com {len(jogo['jogadores'][0])} peça(s)")
-        mao_str = ""
-        mao = jogo['jogadores'][0]
-        for i in range(len(mao)):
-            p = mao[i]
-            mao_str += f"[{p[0]}|{p[1]}] "
-        print(mao_str)
-    
-    
-    jogando = False
-
 while verifica_ganhador(jogo['jogadores']) == -1:
     print("\nMESA:")
     mesa_formatada = ""

96dac675ab2842ab3e301f20ffd02e251b8ce51b
Luka <lukacb@al.insper.edu.br>
2026-01-30 15:39:03 -0300

    escolha de peca e logica do monte

--- jogo.py
+++ jogo.py
@@ -27,3 +27,43 @@ while jogando:
     
     
     jogando = False
+
+while verifica_ganhador(jogo['jogadores']) == -1:
+    print("\nMESA:")
+    mesa_formatada = ""
+    for p in jogo['mesa']:
+        mesa_formatada += f"[{p[0]}|{p[1]}]"
+    print(mesa_formatada if mesa_formatada else "Vazia")
+
+    possiveis = posicoes_possiveis(jogo['mesa'], jogo['jogadores'][0])
+    
+    if jogador_atual == 0:
+        print(f"Jogador: Você com {len(jogo['jogadores'][0])} peça(s)")
+        mao = jogo['jogadores'][0]
+        mao_str = ""
+        for i in range(len(mao)):
+            mao_str += f"[{mao[i][0]}|{mao[i][1]}] "
+        print(mao_str)
+
+        if len(possiveis) > 0:
+            escolha = int(input(f"Escolha a peça (1-{len(mao)}): ")) - 1
+            
+            peca_valida = False
+            for p_pos in possiveis:
+                if escolha == p_pos:
+                    peca_valida = True
+            
+            while not peca_valida:
+                escolha = int(input("Inválido. Escolha outra: ")) - 1
+                for p_pos in possiveis:
+                    if escolha == p_pos:
+                        peca_valida = True
+            
+            peca_escolhida = jogo['jogadores'][0].pop(escolha)
+            jogo['mesa'] = adiciona_na_mesa(peca_escolhida, jogo['mesa'])
+        else:
+            if len(jogo['monte']) > 0:
+                print("Sem peças! Pegando do monte...")
+                jogo['jogadores'][0].append(jogo['monte'].pop(0))
+            else:
+                print("Passando a vez...")
